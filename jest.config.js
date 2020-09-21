b8d3241b376155db3064287059152c4a2c21b2ea
Michael Norton <michael.norton@salesforce.com>
2020-09-21 16:48:23 -0700

    Mock components from another namespace

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,7 @@ module.exports = {
   ...jestConfig,
   // add any custom configurations here
   moduleNameMapper: {
+    '^c/displayPanel$': '<rootDir>/force-app/test/jest-mocks/c/displayPanel',
     '^thunder/hammerButton$': '<rootDir>/force-app/test/jest-mocks/thunder/hammerButton',
     '^lightning/button$': '<rootDir>/force-app/test/jest-mocks/lightning/button'
   }
